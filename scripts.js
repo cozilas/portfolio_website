cf4d484ceb6de62f93898301ba30f6625685f172
Christos Eleftheriou <cozilasdev@gmail.com>
2024-08-04 14:38:45 +0300

    Base layout of homepage
    TODO:
    - Colors need to be adjusted.
    - Navigation bar linkes need to be added.
    - Projects tabs need to be fixed.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
 function toggleMobileMenu() {
   document.getElementById("menu").classList.toggle("active");
 }
+
+function enlargeBox(element) {
+  const enlargedElements = document.querySelectorAll(".enlarged");
+
+  if (enlargedElements.length > 0) {
+    enlargedElements.forEach((enlargedElement) => {
+      console.log(
+        `Element ${enlargedElement.innerText} is currently enlarged.`
+      );
+    });
+
+    enlargedElements.forEach((enlargedElement) => {
+      if (enlargedElement !== element) {
+        enlargedElement.classList.remove("enlarged");
+      }
+    });
+  }
+
+  element.classList.toggle("enlarged");
+  console.log("\n");
+}
