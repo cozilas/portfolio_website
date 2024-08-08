9e65b7af12d86e526dbde2764d80fda7c5e53ebe
Christos Eleftheriou <cozilasdev@gmail.com>
2024-08-08 19:34:28 +0300

    Project tabs fixed
    - Tabs now have an arrow to idicate which way the tab can expand.
    - The project dynamicaly change their sizes.
    - The navigation bar buttons set.(home and contacts need to be
      assigned.

--- scripts.js
+++ scripts.js
@@ -15,10 +15,26 @@ function enlargeBox(element) {
     enlargedElements.forEach((enlargedElement) => {
       if (enlargedElement !== element) {
         enlargedElement.classList.remove("enlarged");
+        enlargedElement.style.height = 300 + "px";
       }
     });
   }
 
   element.classList.toggle("enlarged");
+
+  let totalHeight = 0;
+  const children = element.children;
+  for (let i = 0; i < children.length; i++) {
+    totalHeight += children[i].scrollHeight;
+  }
+
+  const margin = 75;
+  if (element.classList.contains("enlarged")) {
+    element.style.height = margin + totalHeight + "px";
+  } else {
+    element.style.height = 300 + "px";
+  }
+
+  console.log(`Width of element: ${totalHeight}px`);
   console.log("\n");
 }
